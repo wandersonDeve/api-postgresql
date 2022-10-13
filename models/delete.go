e621841f6f19a95e6f61b19317edbecbfd795c79
Wanderson Santos <81826043+wandersonDeve@users.noreply.github.com>
2022-10-13 02:56:28 -0300

    fix: sql

--- models/delete.go
+++ models/delete.go
@@ -9,7 +9,7 @@ func Delete(id int64) (int64, error) {
 	}
 	defer conn.Close()
 
-	res, err := conn.Exec(`DELETE FROM todos WHERE id=$`, id)
+	res, err := conn.Exec(`DELETE FROM todos WHERE id=$1`, id)
 	if err != nil {
 		return 0, err
 	}
--- models/update.go
+++ models/update.go
@@ -9,7 +9,7 @@ func Update(id int64, todo Todo) (int64, error) {
 	}
 	defer conn.Close()
 
-	res, err := conn.Exec(`UPDATE todo SET title=$1, description=$2, done=$3 WHERE id=$4`,
+	res, err := conn.Exec(`UPDATE todos SET title=$1, description=$2, done=$3 WHERE id=$4`,
 		todo.Title, todo.Description, todo.Done, id)
 	if err != nil {
 		return 0, err
