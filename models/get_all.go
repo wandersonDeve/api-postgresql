2543cfdc79c414d12b354233f70d1f6cdc8e8792
Wanderson Santos <81826043+wandersonDeve@users.noreply.github.com>
2022-10-13 01:21:55 -0300

    fix: get all todos

--- models/get_all.go
+++ models/get_all.go
@@ -2,7 +2,7 @@ package models
 
 import "api-postgresql/db"
 
-func GetAll(todos []Todo, err error) {
+func GetAll() (todos []Todo, err error) {
 	conn, err := db.OpenConnection()
 	if err != nil {
 		return
@@ -11,19 +11,19 @@ func GetAll(todos []Todo, err error) {
 
 	rows, err := conn.Query(`SELECT * FROM todos`)
 	if err != nil {
-        return
-    }
+		return
+	}
 
 	for rows.Next() {
-        var todo Todo
+		var todo Todo
 
 		err = rows.Scan(&todo.ID, &todo.Title, &todo.Description, &todo.Done)
 		if err != nil {
-            continue
-        }
+			continue
+		}
 
 		todos = append(todos, todo)
 	}
 
-	
+	return
 }
